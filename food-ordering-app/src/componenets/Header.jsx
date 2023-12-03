5c1e40a46403caad0de7dd60002f2c6f6281c8fe
Georgiy Garenov <g_garenov@outlook.com>
2023-12-03 09:00:22 +0200

    Header components was created.

---
+++ food-ordering-app/src/componenets/Header.jsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <nav>
+      <div className="container">
+        <div className="logo-wrapper">
+          <img src={"/"} alt="logo" />
+        </div>
+        <div className="nav-menu-wrapper">
+          <div>Home</div>
+          <div>About</div>
+        </div>
+      </div>
+    </nav>
+  );
+};
