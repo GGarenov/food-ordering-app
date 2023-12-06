4aef79e9bbf388161644577777dce695861aa575
Georgiy Garenov <g_garenov@outlook.com>
2023-12-06 18:06:51 +0200

    Error fetching the data

--- food-ordering-app/src/pages/Menu/index.jsx
+++ food-ordering-app/src/pages/Menu/index.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts, selectAllProducts } from "../../stores/menu/productsSlice";
+
 const Menu = () => {
+  const dispatch = useDispatch();
+  const products = useSelector(selectAllProducts);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return <div className="text-white"> Menu</div>;
 };
 
