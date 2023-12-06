70b3ae4aaefc00d775539133073a3ae7a59c5a7c
Georgiy Garenov <g_garenov@outlook.com>
2023-12-06 10:19:01 +0200

    Delete useNewUrlParser because we are getting a warning.

--- food-ordering-app/server/database/index.js
+++ food-ordering-app/server/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/food-ordering", { useNewUrlParser: true }).catch((e) => {
+mongoose.connect("mongodb://127.0.0.1:27017/food-ordering").catch((e) => {
   console.error("Connection error", e.message);
 });
 
