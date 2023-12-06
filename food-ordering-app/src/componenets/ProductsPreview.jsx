f5410ed96bcd3b25c5c3b0fb39f3de9e21ca43ac
Georgiy Garenov <g_garenov@outlook.com>
2023-12-06 14:27:47 +0200

    react carousel was installed and the products were shown in carusel

--- food-ordering-app/src/componenets/ProductsPreview.jsx
+++ food-ordering-app/src/componenets/ProductsPreview.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { ProductPreviewCard } from "./ProductPreviewCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export const ProductsPreview = () => {
   const [products, setProducts] = useState([]);
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
   useEffect(() => {
     fetch("http://localhost:8080/api/products")
@@ -14,15 +35,16 @@ export const ProductsPreview = () => {
 
   return (
     <div className="container mx-auto pb-4 w-2/3 text-blue">
-      <h2>Products</h2>
-      {products.length > 0 &&
-        products.map((product, index) => {
-          return (
-            <div key={index} className="w-full p-3">
-              <ProductPreviewCard product={product} />
-            </div>
-          );
-        })}
+      <Carousel responsive={responsive}>
+        {products.length > 0 &&
+          products.map((product, index) => {
+            return (
+              <div key={index} className="w-full p-3">
+                <ProductPreviewCard product={product} />
+              </div>
+            );
+          })}
+      </Carousel>
     </div>
   );
 };
