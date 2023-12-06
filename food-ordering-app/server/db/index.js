4ce96f4ca1977a147f943798b92a9efb94d60b1b
Georgiy Garenov <g_garenov@outlook.com>
2023-12-06 10:59:02 +0200

    Database is connected successfully

--- food-ordering-app/server/db/index.js
+++ food-ordering-app/server/db/index.js
@@ -4,6 +4,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/food-ordering").catch((e) => {
   console.error("Connection error", e.message);
 });
 
-const database = mongoose.connection;
+const db = mongoose.connection;
 
-module.exports = database;
+module.exports = db;
