b96b0928465e02384888d46706d9d863a066cb48
Georgiy Garenov <g_garenov@outlook.com>
2023-12-07 17:40:42 +0200

    A lot of problems register the user

--- food-ordering-app/src/pages/Register/index.jsx
+++ food-ordering-app/src/pages/Register/index.jsx
@@ -5,6 +5,7 @@ import { app } from "../../firebase-config";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
   let navigate = useNavigate();
